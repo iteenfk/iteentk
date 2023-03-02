70076c2f16fda305fd70818256ca8c12a3875fcc
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-03-02 18:09:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,39 +1,31 @@
-"use stirict"
+"use strict";
 
-const slider = document.getElementById("slider");
-const btn = document.getElementById("btn");
+const thumbnails = document.querySelectorAll(".thumbnail");
+const mainImage = document.getElementById("main-image");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+let activeIndex = 0;
 
-slider.addEventListener("input", () =>{
-    const passwardLength = document.getElementById("passward-length");
-
-    passwardLength.textContent = slider.value;
+thumbnails[0].addEventListener("click", () => {
+    activeIndex = 0;
+    mainImage.src = thumbnails[0].src;
+    thumbnails[0].classList.add("active");
+    thumbnails[1].classList.remove("active");
+    thumbnails[2].classList.remove("active");
 });
 
-btn.addEventListener("click", () =>{
-    const result = document.getElementById("result");
-    const numbersCheckbox = document.getElementById("numbers-checkbox");
-    const symbolsCheckbox = document.getElementById("symbols-checkbox");
+thumbnails[1].addEventListener("click", () => {
+    activeIndex = 1;
+    mainImage.src = thumbnails[1].src;
+    thumbnails[0].classList.remove("active");
+    thumbnails[1].classList.add("active");
+    thumbnails[2].classList.remove("active");
+});
 
-    const letters ="abcdefghijklmnopqrstuvwxyz";
-    const numbers ="0123456789";
-    const symbols ="&'(=~><\/*};[@[";
-
-    let passward = "";
-    let seed = letters + letters.toUpperCase();
-
-    if (numbersCheckbox.checked === true){
-        seed += numbers;
-    }
-
-    if (symbolsCheckbox.checked === true){
-        seed += symbols;
-    }
-    
-    
-    
-    for (let i = 0; i < slider.value; i++){
-        passward += seed[Math.floor(Math.random() * seed.length)];
-    }
-
-    result.textContent = passward;
+thumbnails[2].addEventListener("click", () => {
+    activeIndex = 2;
+    mainImage.src = thumbnails[2].src;
+    thumbnails[0].classList.remove("active");
+    thumbnails[1].classList.remove("active");
+    thumbnails[2].classList.add("active");
 });
